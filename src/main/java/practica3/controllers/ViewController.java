a9371c5138acd7bcb9fa5f08cc02d512ee7e1d68
calamarte <calamarte@gmail.com>
2018-02-17 18:35:16 +0100

    creo que cabecera

--- src/main/java/practica3/controllers/ViewController.java
+++ src/main/java/practica3/controllers/ViewController.java
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import practica3.exception.CustomExceptionGenerator;
+import practica3.practica3.utils.Utils;
+
+import java.net.SocketException;
 
 
 @Controller
 public class ViewController {
 
     @RequestMapping("/")
-    public ModelAndView home(){
-        return new ModelAndView("index");
+    public ModelAndView home() throws SocketException {
+       ModelAndView jsp = new ModelAndView("index");
+       jsp.addObject("ip", Utils.getFirstNonLoopbackAddress());
+       return jsp;
     }
 
     @ExceptionHandler(CustomExceptionGenerator.class)
