5ba63a573708f43e6fb4d0e0a69ff97beeb87b40
david <davidgarcia227@gmail.com>
2018-02-18 17:36:56 +0100

    Interceptor ya no peta 403

--- src/main/java/practica3/Interceptors/WebInterceptor.java
+++ src/main/java/practica3/Interceptors/WebInterceptor.java
@@ -1,6 +1,5 @@
 package practica3.Interceptors;
 
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.slf4j.Logger;
@@ -11,36 +10,33 @@ import javax.servlet.http.HttpServletResponse;
 
 public class WebInterceptor extends HandlerInterceptorAdapter {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(WebInterceptor.class);
+    private static final String CREDENTIALS_NAME = "Access-Control-Allow-Credentials";
+    private static final String ORIGIN_NAME = "Access-Control-Allow-Origin";
+    private static final String METHODS_NAME = "Access-Control-Allow-Methods";
+    private static final String HEADERS_NAME = "Access-Control-Allow-Headers";
+    private static final String MAX_AGE_NAME = "Access-Control-Max-Age";
+    private static final Logger logger = LoggerFactory.getLogger(WebInterceptor.class);
 
     @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object handler) throws Exception {
-        long startTime = System.currentTimeMillis();
-        logger.info("Request URL::" + request.getRequestURL().toString()
-                + ":: Start Time=" + System.currentTimeMillis());
-        request.setAttribute("startTime", startTime);
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("Request URL::" + request.getRequestURL().toString());
+        response.setHeader(CREDENTIALS_NAME, "true");
+        response.setHeader(ORIGIN_NAME, "http://localhost:8080");
+        response.setHeader(METHODS_NAME, "GET, OPTIONS, POST, PUT, DELETE");
+        response.setHeader(HEADERS_NAME, "Content-Type, authorization");
+        response.setHeader(MAX_AGE_NAME, "3600");
+
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request,
-                           HttpServletResponse response, Object handler,
-                           ModelAndView modelAndView) throws Exception {
-        logger.info("Request URL::" + request.getRequestURL().toString()
-                + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("Request URL::" + request.getRequestURL().toString());
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest request,
-                                HttpServletResponse response, Object handler, Exception ex)
-            throws Exception {
-        long startTime = (Long) request.getAttribute("startTime");
-        logger.info("Request URL::" + request.getRequestURL().toString()
-                + ":: End Time=" + System.currentTimeMillis());
-        logger.info("Request URL::" + request.getRequestURL().toString()
-                + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("Request URL::" + request.getRequestURL().toString());
     }
 
 }
