eff1ab47b0a296a2316cbda4720a6490516e4357
david <davidgarcia227@gmail.com>
2018-02-19 17:23:56 +0100

    pruebas mock

--- src/main/java/practica3/Interceptors/WebInterceptor.java
+++ src/main/java/practica3/Interceptors/WebInterceptor.java
@@ -1,14 +1,22 @@
 package practica3.Interceptors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Base64;
+import practica3.login.AuthLogin;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.Authenticator;
 
+@Component
 public class WebInterceptor extends HandlerInterceptorAdapter {
+    @Autowired
+    AuthLogin checkLogin;
 
     private static final String CREDENTIALS_NAME = "Access-Control-Allow-Credentials";
     private static final String ORIGIN_NAME = "Access-Control-Allow-Origin";
@@ -21,10 +29,35 @@ public class WebInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("Request URL::" + request.getRequestURL().toString());
         response.setHeader(CREDENTIALS_NAME, "true");
-        response.setHeader(ORIGIN_NAME, "http://localhost:8080");
+        response.setHeader(ORIGIN_NAME, "*");
         response.setHeader(METHODS_NAME, "GET, OPTIONS, POST, PUT, DELETE");
-        response.setHeader(HEADERS_NAME, "Content-Type, authorization");
+        response.setHeader(HEADERS_NAME, "Origin, Content-Type, accept, Authorization");
         response.setHeader(MAX_AGE_NAME, "3600");
+
+        logger.info(request.getHeader("Content-type"));
+        logger.info(request.getHeader("Authorization"));
+
+//
+//        String headerContent = request.getHeader("Authorization");
+//        logger.info("header 1: "+headerContent);
+
+//        if (!request.getRequestURL().toString().endsWith("/")){
+//            logger.info("no /");
+//            String headerContent2 = request.getHeader("Authorization");
+//            logger.info("header2: "+headerContent);
+//            String[] cypherHeader = headerContent.split(" ");
+//            logger.info("cifrado"+cypherHeader);
+//            byte[] decoded = Base64.getDecoder().decode(cypherHeader[1]);
+//            logger.info("decoded: "+decoded);
+
+//            checkLogin.validate(request.getHeader("Authorization"));
+//        }
+
+//        String[] cypherHeader = headerContent.split(" ");
+//        logger.info(cypherHeader.toString());
+//        byte[] decoded = Base64.getDecoder().decode(cypherHeader);
+//        logger.info("decoded: "+decoded);
+
 
         return true;
     }
